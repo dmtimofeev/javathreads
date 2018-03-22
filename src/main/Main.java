fa8f33d23095774230cb13eeecd9112a8e5ef3a5
AlViFedotov <AlViFedotov@sportmaster.ru>
2018-03-22 10:41:54 +0300

    Первые проблемы: почему не работает?

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,21 +5,25 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final String EXIT_COMMAND = "exit";
+    private static final String STOP_COMMAND = "stop";
+
+    private static boolean stopFlag = false;
+
     private static Runnable task = () -> {
         //Выполняеется в отдельном потоке
         Thread.currentThread().setName("Фоновый поток");
-        int i = 0;
-        while (true){
-            sleep(3);
-            System.err.print(getCurrentThreadName());
-            System.err.println("Считаю что-то в фоновом потоке: " + i++);
+        while (!stopFlag){
+            //якобы усиленно делаем что-то, пока не будет установлен флаг остановки... и никогда не видим его!
         }
+        System.err.print(getCurrentThreadName());
+        System.err.println("Фоновый поток обнаружил флаг остановки и завершился");
     };
 
     public static void main(String[] args) {
         Thread.currentThread().setName("Основной поток");
 
         System.out.println("Type \"" + EXIT_COMMAND + "\" for exit.");
+        System.out.println("Type \"" + STOP_COMMAND + "\" for stopping background thread.");
         Scanner scanner = new Scanner(System.in);
 
         Thread thread = new Thread(task);
@@ -31,6 +35,11 @@ public class Main {
                 System.exit(0);
             }
             System.out.print(getCurrentThreadName());
+            if(message.equalsIgnoreCase(STOP_COMMAND)){
+                stopFlag = true;
+                System.out.println("Установлен флаг остановки фонового процесса: stopFlag=" + stopFlag);
+                continue;
+            }
             System.out.println("Кто сказал \"" +  message + "\"?");
         }
     }
