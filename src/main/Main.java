b9e619463584d3e381a5c9566d70d68b87a142df
AlViFedotov <AlViFedotov@sportmaster.ru>
2018-03-22 11:10:11 +0300

    synchronized: первое знакомство.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,26 +4,17 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private static final String EXIT_COMMAND = "exit";
-    private static final String STOP_COMMAND = "stop";
-
-    private static volatile boolean stopFlag = false;
+    private static final Object MONITOR = new Object();
 
     private static Runnable task = () -> {
-        //Выполняеется в отдельном потоке
-        Thread.currentThread().setName("Фоновый поток");
-        while (!stopFlag){
-            //якобы усиленно делаем что-то, пока не будет установлен флаг остановки... и никогда не видим его!
+        int i = 0;
+        while (true){
+            print("Что-то делаем в фоне: " + i++, "Background");
+            sleep(1);
         }
-        System.err.print(getCurrentThreadName());
-        System.err.println("Фоновый поток обнаружил флаг остановки и завершился");
     };
 
     public static void main(String[] args) {
-        Thread.currentThread().setName("Основной поток");
-
-        System.out.println("Type \"" + EXIT_COMMAND + "\" for exit.");
-        System.out.println("Type \"" + STOP_COMMAND + "\" for stopping background thread.");
         Scanner scanner = new Scanner(System.in);
 
         Thread thread = new Thread(task);
@@ -31,21 +22,19 @@ public class Main {
 
         while (true){
             String message = scanner.nextLine();
-            if(message.equalsIgnoreCase(EXIT_COMMAND)){
-                System.exit(0);
-            }
-            System.out.print(getCurrentThreadName());
-            if(message.equalsIgnoreCase(STOP_COMMAND)){
-                stopFlag = true;
-                System.out.println("Установлен флаг остановки фонового процесса: stopFlag=" + stopFlag);
-                continue;
-            }
-            System.out.println("Кто сказал \"" +  message + "\"?");
+            print(message, "Main");
+            sleep(1);
         }
     }
 
-    private static String getCurrentThreadName(){
-        return "[" + Thread.currentThread().getName() + "] ";
+    private static void print(String message, String who){
+        synchronized (MONITOR){
+            System.err.println(who + " thread print start...");
+            sleep(3);
+            System.out.println(who + ": " + message);
+            sleep(3);
+            System.err.println(who + " thread print end.");
+        }
     }
 
     private static void sleep(int sleepSeconds) {
