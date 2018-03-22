73a50fe8f71792df64d21fcc54225d6992f5bc2b
AlViFedotov <AlViFedotov@sportmaster.ru>
2018-03-22 11:38:16 +0300

    Когда поток скучает.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,34 +4,49 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final String TOUCH_COMMAND = "тык";
+    private static final String AWAY_COMMAND = "уходи";
+    private static final Object MONITOR = new Object();
 
     private static Runnable task = () -> {
         int i = 0;
+        System.out.println("И кому я тут понадобился? Начнём с " + i++);
         while (true){
-            print("Что-то делаем в фоне: " + i++, "Background");
-            sleep(1);
+            System.out.println("Z-z-z...");
+            synchronized (MONITOR){
+                try {
+                    MONITOR.wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Злые вы, уйду я от вас.");
+                    return;
+                }
+            }
+            System.out.println("Да не сплю я! Вот, посчитал даже: " + i++);
         }
     };
 
     public static void main(String[] args) {
+        System.out.println("Начепятайте \"" + TOUCH_COMMAND +"\" чтобы разбудить фоновый поток.");
+        System.out.println("Напепятайте \"" + AWAY_COMMAND +"\" дабы прервать фоновый поток.");
         Scanner scanner = new Scanner(System.in);
 
-        Thread thread = new Thread(task);
-        thread.start();
+        Thread thread = null;
 
         while (true){
             String message = scanner.nextLine();
-            print(message, "Main");
-            sleep(1);
+            if(thread == null || thread.getState() == Thread.State.TERMINATED) {
+                thread = new Thread(task);
+                thread.start();
+            }
+            if(message.equalsIgnoreCase(TOUCH_COMMAND)){
+                synchronized (MONITOR){
+                    MONITOR.notify();
+                }
+            }
+            if(message.equalsIgnoreCase(AWAY_COMMAND)){
+                thread.interrupt();
+            }
         }
-    }
-
-    private static synchronized void print(String message, String who){
-        System.err.println(who + " thread print start...");
-        sleep(3);
-        System.out.println(who + ": " + message);
-        sleep(3);
-        System.err.println(who + " thread print end.");
     }
 
     private static void sleep(int sleepSeconds) {
